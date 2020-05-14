7774354e39dae85b9a8b27f24a530c8944a68532
Nikita Tremasov <nikita.triemasov@mail.ru>
2020-05-14 16:48:04 +0500

    ymnoj

--- Сложение.pas
+++ Сложение.pas
@@ -1,10 +1,12 @@
 ﻿var
- a,b,c:integer;
+ a,b,c,z:integer;
  begin
   writeln('Напишите первое число');
   read(a);
   writeln('Напишите ведите второе число число');
   read(b);
   c:=a+b;
+  z:=a*b;
   writeln('Ответ ',c);
+  writeln('Ответ',z);
  end.
