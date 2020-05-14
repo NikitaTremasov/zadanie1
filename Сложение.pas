648686b0c5551f39a70aa5e9bac6e9585d898338
Nikita Tremasov <nikita.triemasov@mail.ru>
2020-05-14 16:45:46 +0500

    summa

---
+++ Сложение.pas
@@ -0,0 +1,10 @@
+﻿var
+ a,b,c:integer;
+ begin
+  writeln('Напишите первое число');
+  read(a);
+  writeln('Напишите ведите второе число число');
+  read(b);
+  c:=a+b;
+  writeln('Ответ ',c);
+ end.
